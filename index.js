8a4bb05944b2b20372a1d73092a93a1672c15a4e
James Halliday <substack@gmail.com>
2011-02-26 04:26:29 -0800

    factored out diffie-hellman code into kex.js, still has packing issues

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var crypto = require('crypto');
-
 var net = require('net');
 var Binary = require('binary');
 var Put = require('put');
-var bigint = require('bigint');
 
 var constants = require('./lib/constants');
 var keyExchange = require('./lib/kex');
@@ -11,7 +8,7 @@ var frame = require('./lib/frame');
 var dss = require('./lib/dss');
 
 module.exports = function (opts) {
-    var gen = dss.generate();
+    var gen = dss.generate(opts.dss);
     return net.createServer(session.bind({}, gen, opts || {}));
 };
 
@@ -41,97 +38,30 @@ function session (gen, opts, stream) {
             }
         })
         .tap(function (vars) {
-            var negotiation = vars.choices.kex_algorithms.serverName;
-            if (negotiation === 'diffie-hellman-group1-sha1') {
-                
+            var algo = vars.choices.kex_algorithms.serverName;
+            var neg = keyExchange.negotiations[algo];
+            vars.ident = ident;
+            
+            if (!neg) {
+                console.error('Unrecognized negotation algorithm ' + algo);
+                stream.end();
+            }
+            else {
                 this
                     .tap(frame.unpack('kexdh'))
                     .tap(function (kvars) {
-                        var kexdh = kvars.kexdh.payload;
-                        if (kexdh[0] !== constants.magic.kexdh_init) {
-                            console.error('Non-kexdh follows'
-                                + ' diffie-hellman negotation');
-                            stream.end();
-                        }
-                        
-                        var e = bigint.fromBuffer(
-                            Binary.parse(kexdh)
-                                .skip(1)
-                                .word32be('length')
-                                .buffer('e', 'length')
-                                .vars.e
-                        );
-                        
-                        var session = gen.session();
-                        
-                        function ipack (buf) {
-                            return Put().word32be(buf.length).put(buf).buffer();
-                        }
-                        
-                        var f = gen.g.powm(session.y, gen.p);
-                        var fbuf = ipack(f.toBuffer());
-                        
-                        var K = e.powm(session.y, gen.p);
-                        var Kbuf = ipack(K.toBuffer());
-                        
-                        var K_S = Put() // K_S
-                            .word32be(opts.dss.pubkey)
-                            .put(new Buffer(opts.dss.pubkey))
-                            .buffer()
-                        ;
-                        
-                        var sign = crypto.createSign('DSA');
-                        
-                        sign.update(Put() // V_S
-                            .word32be(vars.client.version.length)
-                            .put(vars.client.version)
-                            .buffer()
-                        );
-                        
-                        sign.update(Put() // V_C
-                            .word32be(ident.length)
-                            .put(new Buffer(ident))
-                            .buffer()
-                        );
-                        
-                        sign.update(K_S);
-                        
-                        sign.update(Put() // I_C
-                            .word32be(kexdh.length)
-                            .put(kexdh)
-                            .buffer()
-                        );
-                        
-                        sign.update(Put() // I_S
-                            .word32be(vars.keyxRes.buffer.length)
-                            .put(vars.keyxRes.buffer)
-                            .buffer()
-                        );
-                        
-                        sign.update(kexdh.slice(1)) // e
-                        
-                        sign.update(fbuf);
-                        sign.update(Kbuf);
-                        
-                        var signed = new Buffer(
-                            sign.sign(opts.dss.privkey, 'base64'), 'base64'
-                        );
-                        
-                        frame.pack(8, Put()
-                            .word8(constants.magic.kexdh_reply)
-                            .put(K_S)
-                            .put(fbuf)
-                            .word32be(signed.length)
-                            .put(signed)
-                            .buffer()
-                        ).write(stream);
-                        
-                        console.dir(signed);
+                        vars.dh = kvars;
+                        neg(gen, vars, function (err, reply) {
+                            if (err) {
+                                console.error(err);
+                                stream.end();
+                            }
+                            else {
+                                reply.write(stream);
+                            }
+                        });
                     })
                 ;
-            }
-            else {
-                console.error('Unsupported negotiation');
             }
         })
     ;
